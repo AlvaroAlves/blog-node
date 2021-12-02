1cbfbf7ec0c3c576de14024ca528f21ef2fb0ce9
alvaro alves <ctl_aalves@uolinc.com>
2021-12-02 13:21:42 -0300

    instalação slugify

--- categories/CategoriesController.js
+++ categories/CategoriesController.js
@@ -1,8 +1,22 @@
 const express = require("express");
 const router = express.Router();
+const Category = require("./Category");
+const Slugify = require("slugify");
 
 router.get('/admin/categories/new', (req, res) => {
     res.render("admin/categories/new");
+})
+
+router.get('/categories/save', (req, res) => {
+    let title = req.body.title;
+    if(title != undefined){
+        Category.create({
+            title: title,
+            slug: Slugify(title)
+        })
+    }else
+        res.redirect("admin/categories/new");
+    
 })
 
 module.exports = router;
