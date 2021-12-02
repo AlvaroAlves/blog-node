83659e534b382c9549866b36336173a928418402
alvaro alves <ctl_aalves@uolinc.com>
2021-12-02 12:15:03 -0300

    Adicionados models de categoria e artigo

---
+++ articles/Article.js
@@ -0,0 +1,19 @@
+import Sequelize from "sequelize";
+import connection from "../database/database";
+
+const Article = connection.define('articles', {
+    title: {
+        type: Sequelize.STRING,
+        allowNull: false
+    }
+    ,slug: {
+        type: Sequelize.STRING,
+        allowNull: false
+    }
+    ,body: {
+        type: Sequelize.TEXT,
+        allowNull: false
+    }
+})
+
+export default Article;
