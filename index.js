4e388b4464e321a7cc6b65c0dc8a709eb6afab6f
alvaro alves <ctl_aalves@uolinc.com>
2021-12-02 12:02:01 -0300

    adicionado controller de article

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const app = express();
 const bodyParser = require("body-parser")
 const connection = require("./database/database")
 const categoriesController = require("./categories/CategoriesController")
+const articlesController = require("./articles/ArticlesController")
+
 //view engine
 app.set('view engine', 'ejs');
 
@@ -22,11 +24,13 @@ connection
         console.log(error);
     })
 
+//Rotas
 app.use("/", categoriesController);
-
+app.use("/", articlesController);
 app.get("/", (req, res) => {
     res.render("index");
 })
+
 
 app.listen(8080, ()=>{
     console.log("O servidor está rodando!")
